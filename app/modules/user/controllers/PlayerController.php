5479d3526060678ec096704e82f7cb2a9e23d973
greed <littleasiamarket@gmail.com>
2018-11-06 19:47:55 +0700

    edit player sql

--- app/modules/user/controllers/PlayerController.php
+++ app/modules/user/controllers/PlayerController.php
@@ -6,10 +6,14 @@ class PlayerController extends \Backoffice\Controllers\ProtectedController
 
     public function indexAction()
     {
-
+        $sql = 'SELECT * FROM api.user_player WHERE agent_id = '.$this->_user->getId().' AND status = 1 ';
+//        $sql = 'SELECT * FROM api.user_player WHERE status = 1 ';
         $user_player = $this->postgre->query(
-                'SELECT * FROM api.user_player WHERE agent_id = '.$this->_user->getId().' AND status = 1 '
+            $sql
             )->fetchAll();
+//        echo "something <pre>";
+//        var_dump($user_player);
+//        die;
 
         $this->view->user_player = $user_player ;
         \Phalcon\Tag::setTitle("Player List - ".$this->_website->title);
