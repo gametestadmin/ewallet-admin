c4c8bba2cc7ee46cbd7da3fcd5b20d6c163bc906
endmail <endmail123@gmail.com>
2018-11-08 14:51:37 +0700

    new acl

---
+++ app/system/libraries/Security/AccessControlList.php
@@ -0,0 +1,33 @@
+<?php
+namespace System\Library\Security;
+
+use Phalcon\Acl;
+use Phalcon\Acl\Role;
+use Phalcon\Acl\Resource;
+use Phalcon\Mvc\User\Plugin;
+use Phalcon\Events\Event;
+use Phalcon\Mvc\Dispatcher;
+use Phalcon\Acl\Adapter\Memory as AclList;
+
+class AccessControlList extends Plugin
+{
+    public function beforeExecuteRoute(Event $event, Dispatcher $dispatcher)
+    {
+        $module = $this->router->getModuleName();
+        $controller = $dispatcher->getControllerName();
+        $action = $dispatcher->getActionName();
+
+        //check ACL when there is user
+        if($this->session->has('user')){
+            if($this->session->has('acl') && $module != null && $module != 'ajax' ){
+                $acl = $this->session->get('acl') ;
+                if( !isset($acl[$module][$controller]) || !isset($acl[$module][$controller][$action]) || $acl[$module][$controller][$action] == 0){
+                    return $this->response->redirect($_SERVER['HTTP_REFERER'])->send();
+                }
+
+            }
+        }
+
+        return true;
+    }
+}
