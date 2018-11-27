9a767140b01137c7a4a709f960fa291109d1de45
greed <littleasiamarket@gmail.com>
2018-11-28 01:31:48 +0700

    if player_id is null, select all player from database

--- app/system/datalayer/mysql/DLUserPlayerGameAccessLog.php
+++ app/system/datalayer/mysql/DLUserPlayerGameAccessLog.php
@@ -5,8 +5,9 @@ use Backoffice\Controllers\BaseController;
 
 class DLUserPlayerGameAccessLog extends BaseController
 {
-    public function getUserPlayerGamesAccessList($data , $type , $player_id , $agent_id ){
+    public function getUserPlayerGamesAccessList($data , $type , $player_id = null , $agent_id ){
         if ( $data['game'] == 0) $data['game'] = 'game_category';
+        if (is_null($player_id)) $player_id = 'user_player';
         if($type == 9) {
             $sql = "SELECT * FROM api.user_player_game_access_log WHERE user_player = " .$player_id. " 
             AND company = ".$agent_id." AND game_category = ".$data['game']." AND access_time >= ' ".$data['date_start']." ' AND access_time <= ' ".$data['date_end']." '
--- app/system/datalayer/mysql/DLUserPlayerGameTransactionHistory.php
+++ app/system/datalayer/mysql/DLUserPlayerGameTransactionHistory.php
@@ -5,8 +5,9 @@ use Backoffice\Controllers\BaseController;
 
 class DLUserPlayerGameTransactionHistory extends BaseController
 {
-    public function getUserPlayerGamesTransactionHistory($data , $type , $player_id , $agent_id ){
+    public function getUserPlayerGamesTransactionHistory($data , $type , $player_id = null , $agent_id ){
         if ( $data['type'] == 0) $data['type'] = 'type';
+        if ( is_null($player_id)) $player_id = 'user_player';
         if($type == 9) {
             $sql = "SELECT * FROM api.user_player_game_transaction_history WHERE user_player = " .$player_id. " 
             AND company = ".$agent_id." AND type = ".$data['type']." AND transaction_date >= ' ".$data['date_start']." ' AND transaction_date <= ' ".$data['date_end']." '
